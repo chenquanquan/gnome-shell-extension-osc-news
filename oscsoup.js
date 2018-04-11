2e3a7d0ba4e3f262e4755ea97b51512d41d5a0c7
mt6276m <mt6276m@163.com>
2018-04-11 22:46:38 +0800

    Fix the osc soup interface to gnome 2.26 support.
    Signed-off-by: mt6276m <mt6276m@163.com>

--- oscsoup.js
+++ oscsoup.js
@@ -18,56 +18,14 @@ const OscApi = new Lang.Class({
     Name: 'OscNew.OscApi',
 
     _init: function() {
-        //this.initHttp();
+        _httpSession = new Soup.Session();
+        //_httpSession.user_agent = this.user_agent;
     },
 
-    initHttp: function() {
-        this.authUri = new Soup.URI('https://www.oschina.net/action/oauth2/authorize');
-
-    },
-
-    sendTweet: function(id, message) {
-         if (_httpSession === undefined) {
-            _httpSession = new Soup.Session();
-            //_httpSession.user_agent = this.user_agent;
-        }
-        let params = {
-            access_token: "457bb1e6-c6df-43eb-98d8-1bfb54491c03",
-            msg: message
-        };
+    sendMessage: function(id, uri, params, func) {
         let here = this;
+        let message = Soup.form_request_new_from_hash('GET', uri, params);
 
-        let message = Soup.form_request_new_from_hash('GET', tweet_pub_uri, params);
-        if (this.asyncSession === undefined)
-            this.asyncSession = {};
-
-        if (this.asyncSession[id] !== undefined && this.asyncSession[id]) {
-            _httpSession.abort();
-            this.asyncSession[id] = 0;
-        }
-
-        this.asyncSession[id] = 1;
-        _httpSession.queue_message(message, function(_httpSession, message) {
-            here.asyncSession[id] = 0;
-            /* error check */
-            return 0;
-        });
-    },
-
-    getMessageDebug: function(id, fun) {
-        if (_httpSession === undefined) {
-            _httpSession = new Soup.Session();
-            //_httpSession.user_agent = this.user_agent;
-        }
-        let params = {
-            access_token: "457bb1e6-c6df-43eb-98d8-1bfb54491c03",
-            pagesize: "2",
-            page: "1",
-            datatype: "json"
-        };
-        let here = this;
-
-        let message = Soup.form_request_new_from_hash('GET', debug_uri, params);
         if (this.asyncSession === undefined)
             this.asyncSession = {};
 
@@ -80,18 +38,36 @@ const OscApi = new Lang.Class({
         _httpSession.queue_message(message, function(_httpSession, message) {
             here.asyncSession[id] = 0;
             if (!message.response_body.data) {
-                fun.call(here, 0);
+                func.call(here, 0);
                 return 0;
             }
 
             try {
                 let jp = JSON.parse(message.response_body.data);
-                fun.call(here, jp);
+                func.call(here, jp);
             } catch (e) {
-                fun.call(here, 0);
+                func.call(here, 0);
                 return 0;
             }
             return 0;
         });
+    },
+
+    sendTweet: function(id, message, func) {
+        let params = {
+            access_token: "457bb1e6-c6df-43eb-98d8-1bfb54491c03",
+            msg: message
+        };
+        this.sendMessage(id, tweet_pub_uri, params, func);
+    },
+
+    getMessageDebug: function(id, func) {
+        let params = {
+            access_token: "457bb1e6-c6df-43eb-98d8-1bfb54491c03",
+            pagesize: "2",
+            page: "1",
+            datatype: "json"
+        };
+        this.sendMessage(id, debug_uri, params, func);
     },
 });
